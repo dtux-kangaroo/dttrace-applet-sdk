81a8a752792cf451ef4e67eb0f50610b6939362b
长卿 <changqing@dtstack.com>
2018-10-09 20:50:16 +0800

    打包配置调整

--- rollup.config.js
+++ rollup.config.js
@@ -1,30 +1,43 @@
 import babel from 'rollup-plugin-babel'
+import nodeResolve from 'rollup-plugin-node-resolve'
+import commonjs from 'rollup-plugin-commonjs'
 import {uglify} from 'rollup-plugin-uglify'
+import uglifyEs from 'rollup-plugin-uglify-es';
 import packageJson from './package.json' 
 
-const env = process.env.NODE_ENV
+const formatEnv = process.env.FORMAT_ENV
+const compressEnv = process.env.COMPRESS_ENV 
+const compresser ={
+  uglify,
+  uglifyEs
+}
 const banner =`
   /*!
    * ${packageJson.name} v${packageJson.version}
    * (c) 2018-${new Date().getFullYear()} Rui Chengping
    */`
+
 const config = {
   input: 'src/index.js',
   output: {
-    format: 'umd',
-    name: 'Dttrace',
     banner
   },
   plugins: [
+    nodeResolve(),
     babel({
       exclude: '**/node_modules/**'
-    })
+    }),
+    commonjs()
   ]
 }
 
-if (env === 'production') {
+if(formatEnv === 'es' || formatEnv ==='cjs'){
+  config.output.format=formatEnv
+}
+
+if (compressEnv) {
   config.plugins.push(
-    uglify({
+    compresser[compressEnv]({
       compress: {
         pure_getters: true,
         unsafe: true,
